4915ec621b620a73fad4faf82082b4fba335defa
Madhanraj-97 <smadhanraj1997@gmail.com>
2024-11-09 17:31:24 +0530

    fix the error

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-
 export default defineConfig({
-  optimizeDeps: {
-    include: ['react-router-dom'],
+  plugins: [react()],
+  build: {
+    outDir: 'dist', // This is where the build files will be output
   },
 });
