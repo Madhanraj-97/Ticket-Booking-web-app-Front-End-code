df8fb50b5baeeb826d48563b7ff4c985bb68031a
Madhanraj-97 <smadhanraj1997@gmail.com>
2024-11-09 16:39:17 +0530

    add router dependencies

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import './App.css';
 import AdminLogin from './pages/AdminLogin';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { AdminProvider } from './service/AdminContext';
 import { AdminRegister } from './pages/AdminRegister';
 import { AdminHome } from './pages/AdminHome';
 import Homepage from './pages/Homepage';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
