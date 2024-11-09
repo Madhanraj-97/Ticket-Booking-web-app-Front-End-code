579f650d62e6f31309f1c5e094d88a2c17c1dfe3
Madhanraj-97 <smadhanraj1997@gmail.com>
2024-11-09 14:22:29 +0530

    title change

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css';
+import { StrictMode } from 'react'
 import AdminLogin from './pages/AdminLogin';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { AdminProvider } from './service/AdminContext';
