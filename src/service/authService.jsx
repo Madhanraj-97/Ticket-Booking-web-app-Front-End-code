d8aa371574ab9d00bcfc378e76f691328f9ac5db
Madhanraj-97 <smadhanraj1997@gmail.com>
2024-11-12 16:06:44 +0530

    change api

--- src/service/authService.jsx
+++ src/service/authService.jsx
@@ -1,28 +1,28 @@
 import axios from 'axios';
 
-// const API_URL = 'http://localhost:8080/'; // Backend API endpoint
+const API_URL = 'https://ticketz-booking-app.onrender.com/'; // Backend API endpoint
 
 const registerAdmin = (adminDetails) => {
-  return axios.post('http://localhost:8080/admin', adminDetails); // Admin registration API call
+  return axios.post(`${API_URL}admin`, adminDetails); // Admin registration API call
 };
 
 const loginAdmin = (credentials) => {
-  return axios.get(`http://localhost:8080/admin/login?email=${credentials.adminEmail}&password=${credentials.password}`);
+  return axios.get(`${API_URL}admin/login?email=${credentials.adminEmail}&password=${credentials.password}`);
 };
 
 const getAdminById=(id)=>{
-  return axios.get(`http://localhost:8080/admin?id=${id}`);
+  return axios.get(`${API_URL}admin?id=${id}`);
 }
 
 const saveBus = (bus, id) => {
-  return axios.put(`http://localhost:8080/admin/savebus?id=${id}`, bus);
+  return axios.put(`${API_URL}admin/savebus?id=${id}`, bus);
 };
 
 const modifyBus = (bus,id) => {
-  return axios.put(`http://localhost:8080/admin/updatebus?id=${id}`,bus);
+  return axios.put(`${API_URL}admin/updatebus?id=${id}`,bus);
 };
 const deleteBus=(busid)=>{
-  return axios.put(`http://localhost:8080/admin/deletebus?id=${busid}`)
+  return axios.put(`${API_URL}admin/deletebus?id=${busid}`)
   
   
   
