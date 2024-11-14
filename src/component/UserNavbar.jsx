34c5a1e1cc8361f30f61ef2dc99bf7d25c77d75e
Madhanraj-97 <smadhanraj1997@gmail.com>
2024-11-14 18:29:38 +0530

    add user home template

--- src/component/UserNavbar.jsx
+++ src/component/UserNavbar.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function UserNavbar() {
   return (
-    <div>UserNavbar</div>
+    <div id='usernav'>
+    <ul className="nav-bar">
+        <li><Link className='nav-link' to="/">Home</Link></li>
+      <li><Link className='nav-link' to="/admin/welcome">Account</Link></li>
+    </ul>
+  </div>
   )
 }
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -2,14 +2,15 @@ import React from 'react';
 import UserNavbar from '../component/UserNavbar';
 
 export default function Homepage() {
-  return (
-    UserNavbar
+  return (<>
+    <UserNavbar />
     <div>
-      <section><input type="text" placeholder='from'/></section>
-      <section><input type="text" placeholder='To'/></section>
-      <section><input type="text" placeholder='Date'/></section>
+      <section><input type="text" placeholder='from' /></section>
+      <section><input type="text" placeholder='To' /></section>
+      <section><input type="text" placeholder='Date' /></section>
       <section> <button>Search</button> </section>
-      <section><input type="text" placeholder=''/></section>
-      <section><input type="text" placeholder=''/></section></div>
+      <section><input type="text" placeholder='' /></section>
+      <section><input type="text" placeholder='' /></section></div>
+  </>
   )
 }
